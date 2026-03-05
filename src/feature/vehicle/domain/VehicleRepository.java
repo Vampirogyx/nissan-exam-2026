480efa8cf74ed35f10e5a27ad98983f70862a153
gabriel <gabriel.vegsan@educa.jcyl.es>
2026-03-05 18:45:18 +0100

    Crear GetVehiclesUseCase para obtener los vehiculos añadidos y añadimos el uso al VehicleRepository.

--- src/feature/vehicle/domain/VehicleRepository.java
+++ src/feature/vehicle/domain/VehicleRepository.java
@@ -1,5 +1,9 @@
 package feature.vehicle.domain;
 
+import java.util.ArrayList;
+
 public interface VehicleRepository {
     public void saveVehicle(Vehicle vehicle);
+    public ArrayList<Vehicle> getVehicles();
+
 }
